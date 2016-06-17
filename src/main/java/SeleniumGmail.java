4c9b4c7645d55a32927b0cb39bf56b2fe9ba9018
Alexandr Teplyakov <Teplyakov.in@gmail.com>
2016-06-17 13:36:56 +0300

    2

--- src/main/java/SeleniumGmail.java
+++ src/main/java/SeleniumGmail.java
@@ -1,5 +1,8 @@
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
@@ -14,25 +17,30 @@ public class SeleniumGmail {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com.ua/";
+    //driver = new FirefoxDriver();
+    System.setProperty("webdriver.chrome.driver", "E:\\Sasha\\!!!!!!!!!!!!!!!!!!!!!Текущий_День\\Dom_Mebeli\\Hillel\\chromedriver_win32\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://accounts.google.com";
+    PageFactory.initElements(driver, this);
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testSelenium() throws Exception {
-    driver.get(baseUrl + "/#gfe_rd=cr&gws_rd=ssl");
-    driver.findElement(By.linkText("Почта")).click();
+    driver.get(baseUrl + "/ServiceLogin?sacu=1&scc=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&hl=ru&service=mail#identifier");
+    //driver.findElement(By.linkText("Почта")).click();
     driver.findElement(By.id("Email")).clear();
     driver.findElement(By.id("Email")).sendKeys("teplyakov.in@gmail.com");
+    driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
     driver.findElement(By.id("next")).click();
+    driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
     driver.findElement(By.id("Passwd")).clear();
-    driver.findElement(By.id("Passwd")).sendKeys("t0632263378");
+    driver.findElement(By.id("Passwd")).sendKeys("trololo");
     driver.findElement(By.id("signIn")).click();
     driver.findElement(By.id(":2t")).click();
     driver.findElement(By.cssSelector("a.gb_b.gb_Rb")).click();
     driver.findElement(By.linkText("Ещё")).click();
-    driver.findElement(By.xpath(".//*[@id=':34']")).click();
+    //driver.findElement(By.xpath(".//*[@id=':34']")).click();
   }
 
   @AfterClass(alwaysRun = true)
